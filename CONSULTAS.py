b743c4f91c02b4b3cef57531e30e70f8a28cfd68
Equiposeven <elizabeth.perez@colegioesperanzatlax.com>
2026-04-25 18:55:29 -0600

    Add files via upload

--- CONSULTAS.py
+++ CONSULTAS.py
@@ -22,6 +22,29 @@ def guardar_txt(datos, campo):
             archivo.write(f"{t[campo]}\n")
     print(f"Datos de {campo} guardados en TXT")
     
+def consultar_profundidades():
+    datos = consultar_api()
+    for t in datos:
+        print(f"Profundidad: {t['profundidad']} km")
+    guardar_txt(datos, "profundidad")
+
+def consultar_fechas():
+    datos = consultar_api()
+    for t in datos:
+        print(f"Fecha y hora: {t['fecha']}")
+    guardar_txt(datos, "fecha")
+
+def consultar_magnitudes():
+    datos = consultar_api()
+    for t in datos:
+        print(f"Magnitud: {t['magnitud']}")
+    guardar_txt(datos, "magnitud")
+
+def consultar_lugares():
+    datos = consultar_api()
+    for t in datos:
+        print(f"Lugar: {t['lugar']}")
+    guardar_txt(datos, "lugar")
 
 
 
